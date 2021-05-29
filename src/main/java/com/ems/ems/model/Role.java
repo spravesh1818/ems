fe81f6fad4f8822560bdafeb8ab1faefd21c0149
Pravesh Chapagain <spravesh1818@gmail.com>
2021-05-29 21:10:12 +0545

    Different roles added

--- src/main/java/com/ems/ems/model/Role.java
+++ src/main/java/com/ems/ems/model/Role.java
@@ -3,7 +3,7 @@ package com.ems.ems.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-  ROLE_ADMIN, ROLE_CLIENT;
+  ROLE_ADMIN, ROLE_EMPLOYEE,ROLE_FINANCE,ROLE_HR;
 
   public String getAuthority() {
     return name();
